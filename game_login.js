72cbf8a142e9ed0df85f8d662e72e529dfd87d8e
apk-1234 <81460947+apk-1234@users.noreply.github.com>
2021-07-30 14:29:31 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function addUser()
+{
+    var player1_name=document.getElementById("player1").value;
+    var player2_name=document.getElementById("player2").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+
+    window.location="game_page.html";
+}
